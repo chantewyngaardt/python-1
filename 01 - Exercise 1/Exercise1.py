e46b20c296418c614f94e5d241fd8f7e3dd6baa8
chantewyngaardt <chantexwyngaardt@icloud.com>
2024-11-08 10:03:13 +0200

    Add files via upload

---
+++ 01 - Exercise 1/Exercise1.py
@@ -0,0 +1,13 @@
+# name = input("Enter your name: ")
+# age = input("Enter your age: ")
+# print("Hello, " + name +", " + age + ".")
+
+# length = int(input("Enter the length of a rectangle:  "))
+# width = int(input("Enter the width of a rectangle: "))
+# area = length * width 
+# print(f"The area of the rectangle is: {area}")
+
+# celcius = float(input("Enter a temperature in Celcius: "))
+# fahrenheit = (celcius * 9/5) + 32
+# print(round(fahrenheit, 2))
+# print(f"The temperature is {round(fahrenheit, 2)}")
